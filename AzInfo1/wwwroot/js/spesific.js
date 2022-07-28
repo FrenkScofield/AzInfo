5b9705c079991bd7cd26b3b7ff7471c5e716d6e9
FRENK SCOFIELD <kam_alibeyli@mail.ru>
2022-07-28 16:48:54 +0400

    kURUMSAL VE HIZMETLER LANGUAGE READY

--- AzInfo1/wwwroot/js/spesific.js
+++ AzInfo1/wwwroot/js/spesific.js
@@ -1,101 +1,4 @@
-/*
-//navbar
-var h2 = document.getElementById('main');
-var h3 = document.getElementById('corporative');
-var h4 = document.getElementById('services');
-var h5 = document.getElementById('products');
-var h6 = document.getElementById('refrances');
-var h7 = document.getElementById('communications');
 
-//footer
-var footerazinfotur = document.getElementById('footerazinfotur');
-var azturp = document.getElementById('azturp');
-var footerservice = document.getElementById('footerservice');
-
-var fs1=document.getElementById('fs1');
-var fs2=document.getElementById('fs2');
-var fs3=document.getElementById('fs3');
-var fs4=document.getElementById('fs4');
-var fs5=document.getElementById('fs5');
-var fs6=document.getElementById('fs6');
-var fs7=document.getElementById('fs7');
-var fs8=document.getElementById('fs8');
-
-
-
-  function detectChange(selectObject) {
-    var val = selectObject.value;  
-    if(val == "tr"){
-      h2.innerHTML = "ANASAYFA";
-      h3.innerHTML = "KURUMSAL";
-      h4.innerHTML = "HİZMETLER";
-      h5.innerHTML = "ÜRÜNLER";
-      h6.innerHTML = "REFERANSLAR";
-      h7.innerHTML = "İLETİŞİM";
-
-      footerazinfotur.innerHTML = " AZINFO TÜRKİYE";
-      azturp.innerHTML = "Altayçeşme Mh. Çamlı Sk. No:16 DAP Royal Center A Blk D.57 Maltepe/İstanbul"
-      footerservice.innerHTML = "HIZMETLER"
-
-      fs1.innerHTML = "Su Şartlandırma Hizmeti"
-      fs2.innerHTML = "Laboratuvar Hizmetleri"
-      fs3.innerHTML = "Legionella Analizleri"
-      fs4.innerHTML = "Geri Kazanım"
-      fs5.innerHTML = "Sistemlerin Uzaktan Kontrol Edilmesi"
-      fs6.innerHTML = "Datalogger İle Verilerin Toplanmas"
-      fs7.innerHTML = "Ozonla Bakteri Giderim Sistemleri"
-      fs8.innerHTML = "Nötralizasyon Sistemi"
-
-    
-    
-    }else if(val== "en"){
-      h2.innerHTML = "MAIN";
-      h3.innerHTML = "CORPORATE";
-      h4.innerHTML = "SERVICES";
-      h5.innerHTML = "PRODUCTS";
-      h6.innerHTML = "REFERANCES";
-      h7.innerHTML = "COMMUNUCATION";
-
-      footerazinfotur.innerHTML="AZINFO TURKEY";
-      azturp.innerHTML = "Altayçeşme Mh. Çamlı Sk. No:16 DAP Royal Center A Blk D.57 Maltepe/İstanbul"
-      footerservice.innerHTML = "Services"
-
-      fs1.innerHTML = "Water Conditioning Service"
-      fs2.innerHTML = "Laboratory Services"
-      fs3.innerHTML = "Legionella Analyzes"
-      fs4.innerHTML = "Regain"
-      fs5.innerHTML = "Remote Control of Systems"
-      fs6.innerHTML = ""
-      fs7.innerHTML = ""
-      fs8.innerHTML = ""
-
-    
-    
-    }else if(val == "ru"){
-      h2.innerHTML = "Главная";
-      h3.innerHTML = "О нас";
-      h4.innerHTML = "Услуги  ";
-      h5.innerHTML = "Товары";
-      h6.innerHTML = "Ссылки";
-      h7.innerHTML = "Контакты";
-
-      footerazinfotur.innerHTML="Azinfo Турция ";
-      azturp.innerHTML = "Altayçeşme, Çamlı Sok. No:16 DAP Royal Center A Блок K.13 D.57 34843 Малтепе/Стамбул"
-      footerservice.innerHTML = "Услуги "
-
-      fs1.innerHTML = "Услуги по водоподготовке"
-      fs2.innerHTML = "Лабораторные услуги"
-      fs3.innerHTML = "Анализы легионеллы"
-      fs4.innerHTML = "Переработкa"
-      fs5.innerHTML = "Удаленное управление системами"
-      fs6.innerHTML = "Сбор данных с помощью регистратора данных"
-      fs7.innerHTML = ""
-      fs8.innerHTML = ""
-    
-    }
-  }
-
-  */
   var arrLang = {
     'tr': {
       'ANASAYFA': 'ANASAYFA',
@@ -119,6 +22,34 @@ var fs8=document.getElementById('fs8');
        'esperinceslider6':'Hvac ve Endüstriyel Temizlik Ürünleri',
        'esperinceslider7':'Ters Osmoz (R.O.) Birikinti Önleyici ve Temizlik Ürünleri',
        'esperinceslider8':'Bakteri Uygulamaları',
+
+       //KURUMSAL
+          'Institutional': 'Kurumsal',
+          'mainPage': 'Anasayfa',
+          'azinfoAbout': 'Azinfo Hakkında',
+          'azinfoAboutIteam_1': 'Ülkemizin önemli ve prestijli kuruluşlarını portföyünde bulunduran AZINFO, Türkiye’de su şartlandırma danışmanlığı konusundaki otoritesini hizmet kalitesi ile rakipsizleştirmektedir.',
+          'azinfoAboutIteam_2': 'AZINFO, iş merkezlerinin, alışveriş merkezlerinin, fabrikaların, hastanelerin, üniversitelerin, otellerin içme-kullanım sularında, ısıtma-soğutma sistemlerinde, buhar kazanlarında, soğutma kulelerinde kullanılan kimyasalları ve kontrol ekipmanlarını sürekli ve kaliteli servis hizmeti ile takip ederek müşterilerimizin pahalı ekipman yatırımlarını koruyup, verimli ömrünün uzun olmasını sağlayarak ekonomiye katkı sağlamayı amaç edinmiştir.',
+          'azinfoAboutIteam_3': 'Rakiplerin izlemek zorunda kaldıkları mükemmel standartları oluşturan AZINFO’ un pazardaki bu öncü çizgisinin ardında, müşterileri ile kurduğu güçlü iş birliği ve ortak hedefler vardır.',
+          'azinfoAboutIteam_4': 'Yatırımların verimliliği açısından su şartlandırma danışmanlığı acil ve önemli çözüm olmasının yanı sıra, çevreci yöntemlerin, titiz araştırmacılığın ve ihtiyaca özel çözümlerin önem kazandığı bu özel konuda AZINFO gerçek bir çözüm ortağıdır.',
+
+       //HIZMETLER
+          'services': 'Hizmetler',
+          'servicesDescription': 'Azinfo, iş-alışveriş merkezleri, oteller, tatil köyleri, hastaneler, üniversiteler, endüstriyel işletmeler ve fabrikaların su şartlandırma ihtiyaçlarını sürekli ve nitelikli servis hizmeti ile sağlamaktadır.',
+          'servicesIteamTitle_1': 'Su Şartlandırma Hizmeti',
+          'servicesIteamDescription_1': 'Firmaların sorunlarına göre ihtiyacı olan çözümler üretilir ve uygulamaya alınır. Bu çözümler ve cihazlarımızdan dozaj tanklarına kadar özenle kurmaktayız.',
+          'servicesIteamTitle_2': 'Laboratuvar Hizmetleri',
+          'servicesIteamDescription_2': 'Saha analiz çantamız ile gittiğimiz her firmada laboratuvar ortamı oluşturup özenle analizler yapıyoruz.',
+          'servicesIteamTitle_3': 'Legionella Analizleri',
+          'servicesIteamDescription_3': 'Legionella bakterisi doğadaki su kaynaklarından gelen bakterilerdir, binaların su sistemlerine yerleşir ve uygun ortam bulunca çoğalır.',
+          'servicesIteamTitle_4': 'Geri Kazanım',
+          'servicesIteamDescription_4': 'Soğutma kulesinin blöf suyunun bile inanılmaz bir maliyeti olabilir. Özel sistemler kurularak atık suları geri kazandırıyoruz.',
+          'servicesIteamTitle_5': 'Özel Uygulamalar',
+          'servicesIteamSlogan_1': 'Sistemlerin Uzaktan Kontrol Edilmesi',
+          'servicesIteamDescription_5': 'Sisteminizin güncel değerlerini ister ofisinizden isterseniz evinizden görmenizi ve müdahale etmenizi sağlıyoruz. ',
+          'servicesIteamSlogan_2': 'Datalogger İle Verilerin Toplanması',
+          'servicesIteamDescription_6': 'Datalogger cihazlarımızla iletkenlik, pH gibi cihazlarınızın 4-20 mA çıkış verdiği dataları kaydedebilirsiniz.',
+          'servicesIteamSlogan_3': 'Ozonla Bakteri Giderim Sistemleri',
+          'servicesIteamSlogan_4': 'Nötralizasyon Sistemi',
 
        'Industrial':'Azinfo Endüstriyel',
        'Institutional':'Kurumsal',
@@ -190,6 +121,33 @@ var fs8=document.getElementById('fs8');
     'esperinceslider7':'Reverse Osmosis (R.O.) Antifouling and Cleaning Products',
     'esperinceslider8':'Bacteria Applications',
 
+     //KURUMSAL
+        'Institutional': 'Corporate',
+        'mainPage': 'Home page',
+        'azinfoAbout': 'Azinfo About',
+        'azinfoAboutIteam_1': 'Having important and prestigious institutions of our country in its portfolio, AZINFO makes its authority in water treatment consultancy in Turkey unrivaled with its service quality.',
+        'azinfoAboutIteam_2': 'AZINFO maintains the expensive equipment investments of our customers by following the chemicals and control equipment used in the drinking-use water, heating-cooling systems, steam boilers, cooling towers of business centers, shopping centers, factories, hospitals, universities, hotels with continuous and high quality service. It aims to contribute to the economy by ensuring its longevity.',
+        'azinfoAboutIteam_3': 'Behind this pioneering line of AZINFO, which creates the perfect standards that its competitors have to follow, there are strong cooperation with its customers and common goals.',
+        'azinfoAboutIteam_4': 'In addition to being an urgent and important solution in terms of the efficiency of investments, AZINFO is a real solution partner in this special issue where environmentalist methods, meticulous research and tailor-made solutions are important.',
+    // HİZMETLER
+        'services': 'Services',
+        'servicesDescription': 'Azinfo provides water treatment needs of business-shopping centers, hotels, holiday villages, hospitals, universities, industrial enterprises and factories with continuous and qualified service.',
+        'servicesIteamTitle_1': 'Water Conditioning Service',
+        'servicesIteamDescription_1': 'According to the problems of the companies, the solutions they need are produced and put into practice. We carefully install these solutions and devices from our devices to dosage tanks.',
+        'servicesIteamTitle_2': 'Laboratory Services',
+        'servicesIteamDescription_2': 'With our field analysis bag, we create a laboratory environment in every company we go to and make careful analyzes.',
+        'servicesIteamTitle_3': 'Legionella Analyzes',
+        'servicesIteamDescription_3': 'Legionella bacteria are bacteria that come from water sources in nature, settle in the water systems of buildings and multiply when a suitable environment is found.',
+        'servicesIteamTitle_4': 'Regain',
+        'servicesIteamDescription_4': 'Even blowdown water from a cooling tower can have an unbelievable cost. We recycle wastewater by establishing special systems.',
+        'servicesIteamTitle_5': 'Special Applications',
+        'servicesIteamSlogan_1': 'Remote Control of Systems',
+        'servicesIteamDescription_5': 'We enable you to see and intervene the current values of your system from your office or from your home.',
+        'servicesIteamSlogan_2': 'Data Collection with Datalogger',
+        'servicesIteamDescription_6': 'With our datalogger devices, you can record the data that your devices such as conductivity and pH output 4-20 mA.',
+        'servicesIteamSlogan_3': 'Bacteria Removal Systems with Ozone',
+        'servicesIteamSlogan_4': 'Neutralization System',
+
     'Industrial':'Azinfo Industrial',
     'Institutional':'Institutional',
     'Institutionalp1':'Having important and prestigious institutions of our country in its portfolio, AZINFO makes its authority in water treatment consultancy in Turkey unrivaled with its service quality.',
@@ -258,7 +216,36 @@ var fs8=document.getElementById('fs8');
     'esperinceslider5':'Питьевая вода',
     'esperinceslider6':'HVAC и промышленные чистящие средства',
     'esperinceslider7':'Обратный осмос (RO) Противообрастающие и чистящие средства',
-    'esperinceslider8':'Бактериальные аппликации',
+    'esperinceslider8': 'Бактериальные аппликации',
+
+    //KURUMSAL
+        'Institutional': 'О нас',
+        'mainPage': 'Домашняя страница',
+        'azinfoAbout': 'Азинфо О нас',
+        'azinfoAboutIteam_1': 'Владея портфолио  важных и престижных учреждений нашей страны, мы проводим  консультации по вопросам качества воды и услуг по очистке воды в Турции.',
+        'azinfoAboutIteam_2': 'AZINFO постоянно контролирует химические вещества и контрольное оборудование, используемое в питьевой воде в бизнес-центрах, торговых центрах, фабриках, больницах, университетах, гостиницах, системах отопления и охлаждения, паровых котлах, градирнях, с непрерывным и высококачественным обслуживанием, защищая вложения наших клиентов в дорогостоящее оборудование, и он нацелен на вклад в экономику, обеспечивая его долговечность.',
+        'azinfoAboutIteam_3': 'За этой новаторской линией AZINFO, которая создает безупречные стандарты, которым должны следовать конкуренты, стоит тесное сотрудничество и общие цели со своими клиентами.',
+        'azinfoAboutIteam_4': 'С точки зрения эффективности инвестиций, консультации по водоподготовке являются неотложным и важным решением, а AZINFO – является реальным  партнером по решению этого специального вопроса, где важны экологические методы, тщательные исследования и индивидуальные решения.',
+
+        //HIZMETLER
+        'services': 'Услуги',
+        'servicesDescription': 'Azinfo обеспечивает потребности в водоподготовке бизнес-торговых центров, отелей, курортных поселков, больниц, университетов, промышленных предприятий и заводов с непрерывным и квалифицированным обслуживанием.',
+        'servicesIteamTitle_1': 'Water Conditioning Service',
+        'servicesIteamDescription_1': 'Решения, которые нужны компаниям в соответствии с их проблемами, производятся и внедряются. Ниже вы можете найти фотографии станций водоподготовки, которые мы тщательно смонтировали от этих решений и наших устройств до резервуаров-дозаторов.',
+
+        'servicesIteamTitle_2': 'Лабораторные услуги',
+        'servicesIteamDescription_2': 'С помощью сумки для анализа воды мы создаем лабораторную среду в каждой компании, в которую мы работаем, и тщательно анализируем ее. Вы можете найти фото и видео о них ниже.',
+        'servicesIteamTitle_3': 'Анализ на Легионеллу',
+        'servicesIteamDescription_3': 'Бактерии легионеллы - это бактерии, которые происходят из водных источников в природе, оседают в системах водоснабжения зданий и размножаются при обнаружении подходящей среды. ',
+        'servicesIteamTitle_4': 'Переработка',
+        'servicesIteamDescription_4': 'Даже продувочная вода для градирни может иметь огромные затраты. Мы перерабатываем сточные воды, устанавливая специальные системы.',
+        'servicesIteamTitle_5': 'Специальные приложения',
+        'servicesIteamSlogan_1': 'Удаленное управление системами',
+        'servicesIteamDescription_5': 'Если у вас есть чувствительная система или вы хотите иметь возможность постоянно контролировать свою систему, наши устройства управления удаленным доступом станут тем ответом, который вы ищете',
+        'servicesIteamSlogan_2': 'Сбор данных с помощью регистратора данных',
+        'servicesIteamDescription_6': 'С помощью наших устройств регистрации данных вы можете записывать данные, которые ваши устройства выдают на выходе 4-20 мА, такие как проводимость и pH, легко переносить их в Excel и рисовать их графики.',
+        'servicesIteamSlogan_3': 'Системы удаления бактерий с озоном.',
+        'servicesIteamSlogan_4': 'Система нейтрализации',
 
     'Industrial':'Azinfo Промышленность',
     'Institutional':'Институциональный',
